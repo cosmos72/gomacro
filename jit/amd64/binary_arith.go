4c0e698a8f429b16672c7e762ac20425f3890020
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2018-05-21 03:09:41 +0200

    continue jit

--- jit/amd64/binary_arith.go
+++ jit/amd64/binary_arith.go
@@ -19,7 +19,9 @@ package amd64
 // ---------------- ADD --------------------
 
 func (asm *Asm) IncInt64c(z Bind, val int64) *Asm {
-	if val == int64(int32(val)) {
+	if val == 0 {
+		return asm
+	} else if val == int64(int32(val)) {
 		return asm.Bytes(0x48, 0x81, 0x87).Idx(z).Uint32(uint32(val)) // addq   $val,z(%rdi)
 	}
 	asm.load_rax_const(val)
@@ -30,6 +32,8 @@ func (asm *Asm) IncInt64c(z Bind, val int64) *Asm {
 func (asm *Asm) AddInt64c(z Bind, a Bind, val int64) *Asm {
 	if a == z {
 		return asm.IncInt64c(z, val)
+	} else if val == 0 {
+		return asm.Set(z, a)
 	}
 	asm.load_rax_const(val)
 	asm.Bytes(0x48, 0x03, 0x87).Idx(a) //   addq    a(%rdi),%rax
@@ -74,7 +78,9 @@ func (asm *Asm) AddUint64(z, a, b Bind) *Asm {
 // ---------------- SUB --------------------
 
 func (asm *Asm) DecInt64c(z Bind, val int64) *Asm {
-	if val == int64(int32(val)) {
+	if val == 0 {
+		return asm
+	} else if val == int64(int32(val)) {
 		return asm.Bytes(0x48, 0x81, 0xaf).Idx(z).Int32(int32(val)) // subq   $val,z(%rdi)
 	}
 	asm.load_rax_const(-val)           //   // negate val!
@@ -85,6 +91,8 @@ func (asm *Asm) DecInt64c(z Bind, val int64) *Asm {
 func (asm *Asm) SubInt64c(z Bind, a Bind, val int64) *Asm {
 	if a == z {
 		return asm.DecInt64c(z, val)
+	} else if val == 0 {
+		return asm.Set(z, a)
 	}
 	asm.load_rax_const(-val)           //   // negate val!
 	asm.Bytes(0x48, 0x03, 0x87).Idx(a) //   addq    a(%rdi),%rax
@@ -92,9 +100,21 @@ func (asm *Asm) SubInt64c(z Bind, a Bind, val int64) *Asm {
 	return asm
 }
 
-func (asm *Asm) SubInt64(z, a, b Bind) *Asm {
+func (asm *Asm) DecInt64(z, a Bind) *Asm {
+	if a == z {
+		return asm.Zero(z)
+	}
 	asm.load_rax(a)
-	asm.Bytes(0x48, 0x2b, 0x87).Idx(b) //  subq   b(%rdi),%rax
+	asm.Bytes(0x48, 0x29, 0x87).Idx(z) //   subq    %rax,z(%rdi)
+	return asm
+}
+
+func (asm *Asm) SubInt64(z, a, b Bind) *Asm {
+	if a == z {
+		return asm.DecInt64(z, b)
+	}
+	asm.load_rax(a)
+	asm.Bytes(0x48, 0x2b, 0x87).Idx(b) //   subq    b(%rdi),%rax
 	asm.store_rax(z)
 	return asm
 }
@@ -102,8 +122,13 @@ func (asm *Asm) SubInt64(z, a, b Bind) *Asm {
 func (asm *Asm) DecUint64c(z Bind, val uint64) *Asm {
 	return asm.DecInt64c(z, int64(val))
 }
+
 func (asm *Asm) SubUint64c(z Bind, a Bind, val uint64) *Asm {
 	return asm.SubInt64c(z, a, int64(val))
+}
+
+func (asm *Asm) DecUint64(z, a Bind) *Asm {
+	return asm.DecInt64(z, a)
 }
 
 func (asm *Asm) SubUint64(z, a, b Bind) *Asm {
@@ -113,7 +138,11 @@ func (asm *Asm) SubUint64(z, a, b Bind) *Asm {
 // ---------------- MUL --------------------
 
 func (asm *Asm) MulInt64c(z, a Bind, val int64) *Asm {
-	if val == int64(int32(val)) {
+	if val == 0 {
+		return asm.Zero(z)
+	} else if val == 1 {
+		return asm.Set(z, a)
+	} else if val == int64(int32(val)) {
 		asm.Bytes(0x48, 0x69, 0x87).Idx(a).Int32(int32(val)) //  imul   $val,a(%rdi),%rax
 	} else {
 		asm.load_rax_const(val)
