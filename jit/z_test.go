7f8b83f357e70174a3b33ccda506f0e2075db2ac
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2019-02-16 19:20:30 +0100

    jit: add more tests

--- jit/z_test.go
+++ jit/z_test.go
@@ -27,27 +27,31 @@ const (
 	S1
 )
 
-func SameCode(actual Code, expected Code) bool {
-	if len(actual) != len(expected) {
-		return false
+func CompareCode(actual Code, expected Code) int {
+
+	if n1, n2 := len(actual), len(expected); n1 != n2 {
+		if n1 < n2 {
+			return n1
+		}
+		return n2
 	}
 	for i := range actual {
 		if actual[i] != expected[i] {
-			return false
+			return i
 		}
 	}
-	return true
+	return -1
 }
 
 func TestExpr1(t *testing.T) {
 	var c Comp
 	c.Init()
 	r := MakeReg(c.RLo, Uint64)
-	c.Expr(
-		NewExpr1(
-			NEG, NewExpr1(NOT, r),
-		),
+	e := NewExpr1(
+		NEG, NewExpr1(NOT, r),
 	)
+	c.Expr(e)
+	t.Logf("expr: %v", e)
 	actual := c.code
 	expected := Code{
 		asm.ALLOC, S0, Uint64,
@@ -55,36 +59,119 @@ func TestExpr1(t *testing.T) {
 		asm.NEG2, S0, S0,
 	}
 
-	if !SameCode(actual, expected) {
-		t.Errorf("miscompiled code:\n\texpected %v\n\tactual   %v",
-			expected, actual)
+	if i := CompareCode(actual, expected); i >= 0 {
+		t.Errorf("miscompiled code at index %d:\n\texpected %v\n\tactual   %v",
+			i, expected, actual)
+	} else {
+		t.Log("compiled to", actual)
 	}
 }
 
 func TestExpr2(t *testing.T) {
 	var c Comp
-	c.Init()
 	c7 := MakeConst(7, Uint64)
 	c9 := MakeConst(9, Uint64)
-	r1 := MakeReg(c.RLo, Uint64)
-	r2 := MakeReg(c.RLo+1, Uint64)
-	c.Expr(
-		NewExpr2(
+	for _, archId := range []ArchId{asm.AMD64, asm.ARM64} {
+		c.InitArchId(archId)
+		r1 := MakeReg(c.RLo, Uint64)
+		r2 := MakeReg(c.RLo+1, Uint64)
+		e := NewExpr2(
 			ADD, NewExpr2(MUL, c7, r1), NewExpr2(SUB, c9, r2),
-		),
-	)
-	actual := c.code
-	expected := Code{
-		asm.ALLOC, S0, Uint64,
-		asm.MUL3, c7, r1, S0,
-		asm.ALLOC, S1, Uint64,
-		asm.SUB3, c9, r2, S1,
-		asm.ADD3, S0, S1, S0,
-		asm.FREE, S1, asm.Uint64,
-	}
+		)
+		c.Expr(e)
+		t.Logf("expr2: %v", e)
 
-	if !SameCode(actual, expected) {
-		t.Errorf("miscompiled code:\n\texpected %v\n\tactual   %v",
-			expected, actual)
+		actual := c.code
+		expected := Code{
+			asm.ALLOC, S0, Uint64,
+			asm.MUL3, c7, r1, S0,
+			asm.ALLOC, S1, Uint64,
+			asm.SUB3, c9, r2, S1,
+			asm.ADD3, S0, S1, S0,
+			asm.FREE, S1, asm.Uint64,
+		}
+
+		if i := CompareCode(actual, expected); i >= 0 {
+			t.Errorf("miscompiled code at index %d:\n\texpected %v\n\tactual   %v",
+				i, expected, actual)
+		} else {
+			t.Log("compiled to", archId, actual)
+		}
+	}
+}
+
+func TestCompileStmt1(t *testing.T) {
+	var c Comp
+	var s0 SoftRegId
+	for _, archId := range []ArchId{asm.AMD64, asm.ARM64} {
+		c.InitArchId(archId)
+
+		m1 := c.MakeVar(0, Uint64)
+		m2 := c.MakeVar(1, Uint32)
+		m3w := c.MakeVar(2, Uint16)
+		m3 := c.MakeVar(2, Uint8)
+		m4 := c.MakeVar(3, Uint8)
+
+		ts := []Stmt{
+			NewStmt1(INC, m1),                           // m1++
+			NewStmt1(DEC, m2),                           // m2--
+			NewStmt1(ZERO, m3),                          // m3 = 0
+			NewStmt2(ASSIGN, m3w, NewExpr1(UINT16, m3)), // m3w = uint16(m3)
+			NewStmt1(NOP, m4),                           // _ = m4
+			NewStmt2(ASSIGN, m4, m3),                    // m4 = m3
+		}
+		c.Compile(ts...)
+		actual := c.Code()
+		t.Logf("stmt: %v", ts)
+		expected := Code{
+			asm.INC, m1,
+			asm.DEC, m2,
+			asm.ZERO, m3,
+			// asm.NOP, m4, // NOP is optimized away
+			asm.ALLOC, s0, asm.Uint16,
+			asm.CAST, m3, s0,
+			asm.MOV, s0, m3w,
+			asm.FREE, s0, asm.Uint16,
+			asm.MOV, m3, m4,
+		}
+
+		if i := CompareCode(actual, expected); i >= 0 {
+			t.Errorf("miscompiled code at index %d:\n\texpected %v\n\tactual   %v",
+				i, expected, actual)
+		} else {
+			t.Log("compiled to", archId, actual)
+		}
+	}
+}
+
+func TestCompileStmt2(t *testing.T) {
+	var c Comp
+	_7 := MakeConst(7, Uint64)
+	for _, archId := range []ArchId{asm.AMD64, asm.ARM64} {
+		c.InitArchId(archId)
+		s0 := c.AllocSoftReg(Uint64)
+		s1 := c.AllocSoftReg(Uint64)
+		sid0, sid1 := s0.Id(), s1.Id()
+
+		stmt := NewStmt2(ASSIGN, s0,
+			NewExpr1(NEG,
+				NewExpr2(MUL, s1, _7)),
+		)
+		c.Stmt(stmt)
+		actual := c.Code()
+		t.Logf("stmt: %v", stmt)
+		expected := Code{
+			asm.ALLOC, sid0, asm.Uint64,
+			asm.ALLOC, sid1, asm.Uint64,
+			asm.MUL3, sid1, _7, sid0,
+			asm.NEG2, sid0, sid0,
+		}
+
+		if i := CompareCode(actual, expected); i >= 0 {
+			t.Errorf("miscompiled code at index %d:\n\texpected %v\n\tactual   %v",
+				i, expected, actual)
+		} else {
+			t.Log("compiled to", archId, actual)
+		}
 	}
 }
