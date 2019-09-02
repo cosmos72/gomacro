22ac6649e0a88dc5b37ce53dcbc067c2f8442837
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2019-09-02 21:21:05 +0200

    gls: add stub GoID() function for GOARCH=wasm

--- gls/api_wasm.go
+++ gls/api_wasm.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 // +build gc
-// +build !wasm
+// +build wasm
 
 package gls
 
@@ -12,4 +12,9 @@ package gls
 // note that the returned value is DIFFERENT from most other goroutine libraries:
 // this GoID() returns the address, converted to uintptr, of the runtime.g struct.
 // NOT the runtime.g.goid field returned by most other libraries.
-func GoID() uintptr
+func GoID() uintptr {
+	// stub implementation, WASM does not have threads yet.
+	// Question: does GOARCH=wasm support goroutines?
+	// If yes, than a stub implementation is not enough.
+	return 0
+}
